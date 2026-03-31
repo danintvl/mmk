ceb02af8f6289e752dba4fb00b966dd2a5a3ae36
Dani <daniel.nativel@etu.univ-amu.fr>
2026-03-31 19:32:17 +0200

    Added Field class

--- mmk-backend/src/main/java/com/dani/mmk/backend/model/Field.java
+++ mmk-backend/src/main/java/com/dani/mmk/backend/model/Field.java
@@ -14,4 +14,25 @@ public class Field{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable=false, unique=false)
+    private String name;
+
+    @Column(nullable = false, unique = false)
+    private String location;
+
+    public Field(){}
+
+    public Field(Long id, String name, String location){
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public Long getId(){return this.id;}
+    public String getName(){return this.name;}
+    public String getLocation(){return this.location;}
+
+    public void setId(Long id){this.id = id;}
+    public void setName(String name){this.name = name;}
+    public void setLocation(String location){this.location = location;}
 }
