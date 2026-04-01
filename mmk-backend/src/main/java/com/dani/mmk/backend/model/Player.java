f32793a3494969208b7932d19923127fd225720c
Dani <daniel.nativel@etu.univ-amu.fr>
2026-04-01 12:03:54 +0200

    Player.java done.

---
+++ mmk-backend/src/main/java/com/dani/mmk/backend/model/Player.java
@@ -0,0 +1,35 @@
+package com.dani.mmk.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "players")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    protected Player() {
+    }
+
+    public Player(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
