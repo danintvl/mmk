0e25fd6d4c92488042ced29c8b631bc8ec24cd6d
Dani <daniel.nativel@etu.univ-amu.fr>
2026-04-02 00:46:48 +0200

    class Match done

---
+++ mmk-backend/src/main/java/com/dani/mmk/backend/model/Match.java
@@ -0,0 +1,77 @@
+package com.dani.mmk.backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "matches")
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDateTime scheduledAt;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private MatchStatus status;
+
+    @OneToMany(mappedBy = "match", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MatchParticipant> participants = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "field_id")
+    private Field field;
+
+    //Constructors
+    public Match(){}
+    public Match(LocalDateTime scheduledAt, MatchStatus status){
+        this.scheduledAt = scheduledAt;
+        this.status = status;
+    }
+
+    //Getters
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getScheduledAt() {
+        return scheduledAt;
+    }
+
+    public MatchStatus getStatus() {
+        return status;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public List<MatchParticipant> getParticipants() {
+        return participants;
+    }
+
+    //Setters
+    public void setStatus(MatchStatus status) {
+        this.status = status;
+    }
+
+    public void setScheduledAt(LocalDateTime scheduledAt) {
+        this.scheduledAt = scheduledAt;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public void setParticipants(List<MatchParticipant> participants) {
+        this.participants = participants;
+    }
+}
