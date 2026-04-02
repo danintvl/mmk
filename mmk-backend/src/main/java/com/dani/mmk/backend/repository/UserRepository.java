5f55a60ae954e358ea16ec423e8fb4186a8d3a62
Dani <daniel.nativel@etu.univ-amu.fr>
2026-04-02 12:37:36 +0200

    Added some possibilities to retrieve a list of USers and a list of Players

--- mmk-backend/src/main/java/com/dani/mmk/backend/repository/UserRepository.java
+++ mmk-backend/src/main/java/com/dani/mmk/backend/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.dani.mmk.backend.repository;
 
 import com.dani.mmk.backend.model.User;
+import com.dani.mmk.backend.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +12,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    List<User> findByRole(UserRole role);
+
+    Optional<User> findByUsername(String username);
 }
