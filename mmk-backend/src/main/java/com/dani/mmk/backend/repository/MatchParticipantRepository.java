f5d15f45e8cfac08e26061ca85b38a2e94fb11d0
Dani <daniel.nativel@etu.univ-amu.fr>
2026-04-11 13:42:33 +0200

    Reduced implementations for MVP

--- mmk-backend/src/main/java/com/dani/mmk/backend/repository/MatchParticipantRepository.java
+++ mmk-backend/src/main/java/com/dani/mmk/backend/repository/MatchParticipantRepository.java
@@ -5,12 +5,15 @@ import com.dani.mmk.backend.model.MatchParticipant;
 import com.dani.mmk.backend.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MatchParticipantRepository extends JpaRepository<MatchParticipant, Long> {
 
-    Optional<MatchParticipant> findByMatch(Match match);
+    List<MatchParticipant> findByMatch(Match match);
 
-    Optional<MatchParticipant> findByPlayer(Player player);
+    List<MatchParticipant> findByPlayer(Player player);
+
+    Optional<MatchParticipant> findByMatchAndPlayer(Match match, Player player);
 
 }
--- mmk-backend/src/main/java/com/dani/mmk/backend/repository/MatchmakingRequestRepository.java
+++ mmk-backend/src/main/java/com/dani/mmk/backend/repository/MatchmakingRequestRepository.java
@@ -1,9 +1,10 @@
 package com.dani.mmk.backend.repository;
 
-import com.dani.mmk.backend.model.*;
+import com.dani.mmk.backend.model.MatchmakingRequest;
+import com.dani.mmk.backend.model.Player;
+import com.dani.mmk.backend.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MatchmakingRequestRepository extends JpaRepository<MatchmakingRequest, Long> {
