1d3bb51386e7c88ac6d1504d7ad35555fadda669
Dani <daniel.nativel@etu.univ-amu.fr>
2026-04-11 12:02:40 +0200

    MatchmakingRequestRepostitory implemented

--- mmk-backend/src/main/java/com/dani/mmk/backend/repository/MatchmakingRequestRepository.java
+++ mmk-backend/src/main/java/com/dani/mmk/backend/repository/MatchmakingRequestRepository.java
@@ -1,10 +1,15 @@
 package com.dani.mmk.backend.repository;
 
-import com.dani.mmk.backend.model.Match;
-import com.dani.mmk.backend.model.MatchmakingRequest;
-import com.dani.mmk.backend.model.Player;
+import com.dani.mmk.backend.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface MatchmakingRequestRepository extends JpaRepository<MatchmakingRequest, Long> {}
+public interface MatchmakingRequestRepository extends JpaRepository<MatchmakingRequest, Long> {
+
+    Optional<MatchmakingRequest> findByPlayer(Player player);
+
+    Optional<MatchmakingRequest> findByMatch(Match match);
+
+}
