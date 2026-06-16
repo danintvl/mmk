6e8baeb0faae51d1491e12203ffdf365ab621836
Dani <59149448+danintvl@users.noreply.github.com>
2026-06-16 16:03:00 +0200

    Leaving this class on standby until i figure out which depends on which

--- mmk-backend/src/main/java/com/dani/mmk/backend/service/MatchService.java
+++ mmk-backend/src/main/java/com/dani/mmk/backend/service/MatchService.java
@@ -1,4 +1,12 @@
 package com.dani.mmk.backend.service;
 
+import com.dani.mmk.backend.repository.MatchRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class MatchService {
+
+
 }
